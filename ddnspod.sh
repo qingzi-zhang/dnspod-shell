429c19a5abcd5fca02626f2c23434321ed28ceef
qingzi-zhang <qingzi.zhang@outlook.com>
2024-11-19 19:37:35 +0800

    Update ddnspod.sh Improved code standards slightly Upgraded regex for ip4_filters

--- ddnspod.sh
+++ ddnspod.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
-
+#
 # dnspod-shell: A DDNS Shell Script: ddnspod.sh
+
 AGENT="https://github.com/qingzi-zhang/dnspod-shell"
 
 CONF_FILE="/etc/config/ddnspod"
@@ -9,16 +10,15 @@ LOG_FILE="/var/log/ddnspod/ddnspod.log"
 LOG_LEVEL_ERROR=0
 LOG_LEVEL_VERBOSE=1
 LOG_LEVEL_DEFAULT="${LOG_LEVEL_ERROR}"
-LOG_LEVEL=${LOG_LEVEL:-${LOG_LEVEL_DEFAULT}}
+LOG_LEVEL="${LOG_LEVEL:-${LOG_LEVEL_DEFAULT}}"
 LOG_SIZE=1000000 # Bytes
-LOG_TAG="ddnspod"
+TAG="ddnspod"
 
 # Tencent API 3.0 DescribeRecordList and ModifyDynamicDNS current version: 2021-03-23
 # https://cloud.tencent.com/document/api/1427/56166, https://cloud.tencent.com/document/api/1427/56158
 algorithm="TC3-HMAC-SHA256"
 host="dnspod.tencentcloudapi.com"
-# Common Params. This parameter is not required for this API. (optional)
-region=""
+region="" # Common Params. This parameter is not required for this API. (optional)
 service="dnspod"
 version="2021-03-23"
 
@@ -27,6 +27,7 @@ log_msg() {
   # Check if arguments title ($1) and message ($2) are provided
   if [ -z "$1" ] || [ -z "$2" ]; then
     [ "${LOG_LEVEL}" -eq "${LOG_LEVEL_VERBOSE}" ] && echo "Error: [log_msg] Missing one or more required arguments."
+    # Return a non-zero exit status to indicate an error occurred
     return 1
   fi
 
@@ -36,7 +37,7 @@ log_msg() {
     mkdir -p "$(dirname "${LOG_FILE}")"
     # Log a warning message if the log file creation fails
     if ! touch "${LOG_FILE}" >/dev/null 2>&1 ; then
-      logger -p warn -s -t "${LOG_TAG}" "Failed to create '${LOG_FILE}', please check it out"
+      logger -p warn -s -t "${TAG}" "Failed to create '${LOG_FILE}', please check it out"
       return 1
     fi
   fi
@@ -49,7 +50,64 @@ log_msg() {
 
   # Log the messages with timestamp
   log_time="$(date "+%Y-%m-%d %H:%M:%S")"
-  printf -- "----- %s [%s] -----\n%s\n" "$log_time" "$1" "$2" >> "${LOG_FILE}"
+  printf -- "----- %s [%s] -----\n%s\n" "${log_time}" "$1" "$2" >> "${LOG_FILE}"
+}
+
+# Function to get the network interface IP address via ip command
+query_interface_ip() {
+  if [ "${rec_type}" = "A" ]; then
+    # https://www.iana.org/assignments/iana-ipv4-special-registry/iana-ipv4-special-registry.xhtml
+    #	[RFC791] "This network" 0.0.0.0/8
+    ip4_filter="^0\.(25[0-5]|2[0-4][0-9]|[01]?[0-9]{1,2}\.)"
+    # [RFC1122] "This host on this network" 127.0.0.0/8
+    ip4_filter="${ip4_filter}|^127\."
+    # [RFC6598] Shared Address Space 100.64.0.0/10
+    ip4_filter="${ip4_filter}|^100\.(6[4-9]|[7-9][0-9]|1[0-1][0-9]|12[0-7]\.)"
+    # [RFC3927] Link Local 169.254.0.0/16
+    ip4_filter="${ip4_filter}|^169\.254\."
+    # [RFC1918] Private-Use 10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16
+    ip4_filter="${ip4_filter}|^10\."
+    ip4_filter="${ip4_filter}|^172\.(1[6-9]|2[0-9]|3[01]\.)"
+    ip4_filter="${ip4_filter}|^192\.168\."
+    # [RFC1112] Reserved Address Block 240.0.0.0/4
+    ip4_filter="${ip4_filter}|^240\."
+    # [RFC2544] Benchmarking Address Blocks 198.18.0.0/15
+    ip4_filter="${ip4_filter}|^198\.1[8-9]\."
+    # [RFC5737] IPv4 Address Blocks Reserved for Documentation 198.51.100.0/24, 203.0.113.0/24
+    ip4_filter="${ip4_filter}|^198\.51\.100\.|^203\.0\.113\."
+
+    # Fetches and filters IPv4 address from the specific interface
+    ip_addr="$(ip -4 address show dev "${interface}" | sed -n 's/.*inet \([0-9.]\+\).*scope global.*/\1/p' | grep -Ev "${ip4_filter}")"
+  else
+    # https://www.iana.org/assignments/iana-ipv6-special-registry/iana-ipv6-special-registry.xhtml
+    # [RFC4291] Unspecified Address ::/128
+    ip6_filter="^::$"
+    # [RFC4291] Loopback Address ::1/128
+    ip6_filter="${ip6_filter}|^::1$"
+    # [RFC6052] IPv4-IPv6 Translat 64:ff9b::/96
+    # [RFC8215] IPv4-IPv6 Translat 64:ff9b:1::/48
+    ip6_filter="${ip6_filter}|^64:[fF][fF]9[bB]:"
+    # [RFC6666] Discard-Only Address Block 100::/64
+    ip6_filter="${ip6_filter}|^100::"
+    # [RFC5180][RFC Errata 1752] Benchmarking 2001:2::/48
+    ip6_filter="${ip6_filter}|^2001:2:"
+    # [RFC4193][RFC8190] Unique-Local fc00::/7
+    ip6_filter="${ip6_filter}|^[fF][cdCD][0-9a-fA-F]{2}:"
+    # [RFC4291] Link-Local Unicast fe80::/10
+    ip6_filter="${ip6_filter}|^[fF][eE][8-9a-bA-B][0-9a-fA-F]:"
+
+    # Fetches and filters IPv6 address from the specific interface
+    ip_addr="$(ip -6 address show dev "${interface}" | sed -n 's/.*inet6 \([0-9a-fA-F:]\+\)\/64.*scope global dynamic.*/\1/p' | grep -Ev "${ip6_filter}" | head -n 1)"
+  fi
+
+  # Validate IP address extraction
+  if [ -z "${ip_addr}" ]; then
+    logger -p error -s -t "${TAG}" "${domain_full_name} interface '${interface}' ${ip_version} address extraction failed"
+    return 1
+  fi
+
+  # Adds specific custom suffix
+  [ -z "${suffix}" ] || ip_addr="${ip_addr%::*}:${suffix}"
 }
 
 # Function to get the dynamic DNS IP via nslookup
@@ -61,7 +119,7 @@ query_nslookup_ip() {
   if echo "${cmd_result}" | grep -q "\*\*" ; then
     # Extract error message
     err_msg="$(echo "${cmd_result}" | grep '\*')"
-    logger -p error -s -t "${LOG_TAG}" "${domain_full_name} ${rec_type} [nslookup]: ${err_msg}"
+    logger -p error -s -t "${TAG}" "${domain_full_name} ${rec_type} [nslookup]: ${err_msg}"
     return 1
   fi
 
@@ -70,57 +128,9 @@ query_nslookup_ip() {
 
   # Check if IP address extraction was successful
   if [ -z "${ns_ip_addr}" ]; then
-    logger -p error -s -t "${LOG_TAG}" "${domain_full_name} ${rec_type} [nslookup]: IP address extraction failed"
+    logger -p error -s -t "${TAG}" "${domain_full_name} ${rec_type} [nslookup]: IP address extraction failed"
     return 1
   fi
-}
-
-# Function to get the network interface IP address via ip command
-query_interface_ip() {
-  if [ "$rec_type" = "A" ]; then
-    # https://www.iana.org/assignments/iana-ipv4-special-registry/iana-ipv4-special-registry.xhtml
-    # [RFC1122] Lookback Address 127.0.0.0/8
-    ip4_filter="^127$"
-    # [RFC3927] Link Local Address 169.254.0.0/16
-    # [RFC1918] Special-Use IPv4 Addresses 10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16
-    ip4_filter="$ip4_filter|^10"
-    ip4_filter="$ip4_filter|^172\.(1[6-9]|2[0-9]|3[01])"
-    ip4_filter="$ip4_filter|^192"
-    # [RFC1112] Reserved 240.0.0.0/4
-    ip4_filter="$ip4_filter|^240"
-
-    # Fetches and filters IPv4 address from the specific interface
-    ip_addr="$(ip -4 address show dev "$interface" | sed -n 's/.*inet \([0-9.]\+\).*scope global.*/\1/p' | grep -Ev "${ip4_filter}")"
-  else
-    # https://www.iana.org/assignments/iana-ipv6-special-registry/iana-ipv6-special-registry.xhtml
-    # [RFC4291] Unspecified Address ::/128
-    ip6_filter="^::$"
-    # [RFC4291] Loopback Address ::1/128
-    ip6_filter="$ip6_filter|^::1$"
-    # [RFC6052] IPv4-IPv6 Translat 64:ff9b::/96
-    # [RFC8215] IPv4-IPv6 Translat 64:ff9b:1::/48
-    ip6_filter="$ip6_filter|^64:[fF][fF]9[bB]:"
-    # [RFC6666] Discard-Only Address Block 100::/64
-    ip6_filter="$ip6_filter|^100::"
-    # [RFC5180][RFC Errata 1752] Benchmarking 2001:2::/48
-    ip6_filter="$ip6_filter|^2001:2:"
-    # [RFC4193][RFC8190] Unique-Local fc00::/7
-    ip6_filter="$ip6_filter|^[fF][cdCD][0-9a-fA-F]{2}:"
-    # [RFC4291] Link-Local Unicast fe80::/10
-    ip6_filter="$ip6_filter|^[fF][eE][89a-bA-B][0-9a-fA-F]:"
-
-    # Fetches and filters IPv6 address from the specific interface
-    ip_addr="$(ip -6 address show dev "$interface" | sed -n 's/.*inet6 \([0-9a-fA-F:]\+\)\/64.*scope global dynamic.*/\1/p' | grep -Ev "${ip6_filter}" | head -n 1)"
-  fi
-
-  # Validate IP address
-  if [ -z "${ip_addr}" ]; then
-    logger -p error -s -t "${LOG_TAG}" "Failed attempt to retrieve ${ip_version} address for ${domain_full_name}"
-    return 1
-  fi
-
-  # Adds specific custom suffix
-  [ -z "${suffix}" ] || ip_addr="${ip_addr%::*}:${suffix}"
 }
 
 # Function to handle Tencent API errors
@@ -131,13 +141,12 @@ tencent_api_err() {
   if [ -n "${err_code}" ]; then
     # Extract the error message
     err_msg="$(echo "${api_response}" | sed -n 's/.*"Message":"\([^"]*\)".*/\1/p')"
-    logger -p error -s -t "${LOG_TAG}" "${domain_full_name} ${rec_type} [${action}]: ${err_code}, ${err_msg}"
-    # Return a non-zero exit status to indicate an error occurred
+    logger -p error -s -t "${TAG}" "${domain_full_name} ${rec_type} [${action}]: ${err_code}, ${err_msg}"
     return 1
   fi
 }
 
-# Function to send a request to Tencent Cloud DNSPod API 3.0 services
+# Function to call Tencent Cloud DNSPod API 3.0 services
 # This function is based on the example provided by the external API
 # Source: https://github.com/TencentCloud/signature-process-demo/tree/main/signature-v3/bash/signv3_no_xdd.sh
 tencent_api_req() {
@@ -186,7 +195,7 @@ rec_query() {
   log_msg "${action} request" "${payload}"
   api_response="$(tencent_api_req)"
   log_msg "${action} response" "${api_response}"
-  domain_full_name="${domain_full_name:-"${subdomain}.${domain}"}"
+  domain_full_name="${domain_full_name:-${subdomain}.${domain}}"
   tencent_api_err "${domain_full_name}" "${rec_type}" "${action}" "${api_response}" || return 1
 }
 
@@ -197,7 +206,7 @@ rec_update() {
   log_msg "$action request" "${payload}"
   api_response="$(tencent_api_req)"
   log_msg "${action} response" "${api_response}"
-  domain_full_name="${domain_full_name:-"${subdomain}.${domain}"}"
+  domain_full_name="${domain_full_name:-${subdomain}.${domain}}"
   tencent_api_err "${domain_full_name}" "${rec_type}" "${action}" "${api_response}" || return 1
 }
 
@@ -205,13 +214,7 @@ rec_update() {
 sync_ddns_rec() {
   # Validate required fields
   if [ -z "${domain}" ]; then
-    logger -p error -s -t "${LOG_TAG}" "Missing required field 'domain' in the config file '${CONF_FILE}', check it out."
-    return 1
-  fi
-
-  # Validate network interface
-  if ! ip link ls dev "${interface}" >/dev/null 2>&1 ; then
-    [ "${LOG_LEVEL}" -lt "${LOG_LEVEL_ERROR}" ] || echo "Error: Invalid interface '${interface}' or unavailable"
+    logger -p error -s -t "${TAG}" "Missing required field 'domain' in the config file '${CONF_FILE}', check it out."
     return 1
   fi
 
@@ -226,8 +229,14 @@ sync_ddns_rec() {
     domain_full_name="${subdomain}.${domain}"
   fi
 
+  # Validate network interface
+  if ! ip link ls dev "${interface}" >/dev/null 2>&1 ; then
+    logger -p error -s -t "${TAG}" "${domain_full_name} interface '${interface}' is invalid not available"
+    return 1
+  fi
+
   # Set IP version to IPv6 if not specified in the config file
-  ip_version="${ip_version:-"IPv6"}"
+  ip_version="${ip_version:-'IPv6'}"
   # Convert to special format (IPv4/IPv6)
   ip_version="$(echo "${ip_version}" | sed 's/[iI][pP][vV]/IPv/')"
   # Convert to record type (A/AAAA)
@@ -237,14 +246,14 @@ sync_ddns_rec() {
     rec_type="AAAA"
   fi
 
-  # Get nslookup IP address
-  query_nslookup_ip "${domain_full_name}" "${rec_type}" || return 1
   # Get network interface IP address
   query_interface_ip "${rec_type}" "${interface}" "${suffix}" || return 1
+  # Get nslookup IP address
+  query_nslookup_ip "${domain_full_name}" "${rec_type}" || return 1
 
   # Check if the local IP address is the same as the server IP address
   if [ "${ip_addr}" = "${ns_ip_addr}" ]; then
-    [ "${LOG_LEVEL}" -ne "${LOG_LEVEL_VERBOSE}" ] || logger -p info -s -t "${LOG_TAG}" "${domain_full_name} ${ip_version} address ${ip_addr} is up to date"
+    [ "${LOG_LEVEL}" -ne "${LOG_LEVEL_VERBOSE}" ] || logger -p info -s -t "${TAG}" "${domain_full_name} ${ip_version} address ${ip_addr} is up to date"
     [ "${LOG_LEVEL}" -eq "${LOG_LEVEL_VERBOSE}" ] || echo "${domain_full_name} ${ip_version} address ${ip_addr} is up to date"
 
     # Skip when a force-update is not enabled (The IP address is already the latest)
@@ -258,13 +267,13 @@ sync_ddns_rec() {
   record_id="$(echo "${api_response}" | sed 's/.*"RecordId":\([0-9]*\).*/\1/')"
   record_ip="$(echo "${api_response}" | sed -n 's/.*"Value":"\([0-9a-fA-F.:]*\)".*/\1/p')"
   if [ -z "${record_id}" ] || [ -z "${record_ip}" ]; then
-    logger -p error -s -t "${LOG_TAG}" "Fail attempt to extract RecordId or IP address for ${domain_full_name} ${rec_type} from DNSPod API response"
+    logger -p error -s -t "${TAG}" "Fail attempt to extract RecordId or IP address for ${domain_full_name} ${rec_type} from DNSPod API response"
     return 1
   fi
 
   # If the IP address is up to date here, it means the local DNS cache is out of date
   if [ "${ip_addr}" = "${record_ip}" ]; then
-    [ "${LOG_LEVEL}" -lt "${LOG_LEVEL_ERROR}" ] || logger -p info -s -t "${LOG_TAG}" "${domain_full_name} cache of ${ip_version} address ${ip_addr} is up to date"
+    [ "${LOG_LEVEL}" -lt "${LOG_LEVEL_ERROR}" ] || logger -p info -s -t "${TAG}" "${domain_full_name} cache of ${ip_version} address ${ip_addr} is up to date"
     # Skip when a force-update is not enabled (The IP address cache is already up to date)
     [ "$FORCE_UPDATE" -eq 0 ] && return 0
   fi
@@ -275,7 +284,7 @@ sync_ddns_rec() {
   # Update DDNS (DNSPod API: ModifyDynamicDNS)
   rec_update "${domain}" "${record_id}" "${record_line}" "${ip_addr}" "${subdomain}" || return 1
 
-  logger -p notice -s -t "${LOG_TAG}" "${domain_full_name} ${ip_version} address has been updated to ${ip_addr}"
+  logger -p notice -s -t "${TAG}" "${domain_full_name} ${ip_version} address has been updated to ${ip_addr}"
 }
 
 # Function to parse command line arguments
@@ -308,7 +317,7 @@ parse_command() {
   done
 
   if [ "${FORCE_UPDATE}" -eq 1 ] && [ "${LOG_LEVEL}" -eq "${LOG_LEVEL_VERBOSE}" ]; then
-    printf -- "\n%s: Processing with force update is enabled\n\n" "${LOG_TAG}"
+    printf -- "\n%s: Processing with force update is enabled\n\n" "${TAG}"
   fi
 }
 
@@ -353,19 +362,19 @@ Options:
 validate_config() {
   # Validate the config file
   if [ ! -f "${CONF_FILE}" ]; then
-    logger -p error -s -t "${LOG_TAG}" "Config file '${CONF_FILE}' not found."
+    logger -p error -s -t "${TAG}" "Config file '${CONF_FILE}' not found."
     return 1
   fi
 
   # Exit if no DDNS records are found in the config file.
   if ! grep -q "DDNS" "${CONF_FILE}" ; then
-    logger -p warn -s -t "${LOG_TAG}" "No DDNS records in '${CONF_FILE}'."
+    logger -p warn -s -t "${TAG}" "No DDNS records in '${CONF_FILE}'."
     return 1
   fi
 
   # Validate the log level
   if ! echo "${LOG_LEVEL}" | grep -q "^[01]$" ; then
-    logger -p error -s -t "${LOG_TAG}" "Error: Invalid log level '${LOG_LEVEL}', Use 0 (Error) or 1 (Verbose)"
+    logger -p error -s -t "${TAG}" "Error: Invalid log level '${LOG_LEVEL}', Use 0 (Error) or 1 (Verbose)"
     return 1
   fi
 
@@ -380,14 +389,14 @@ validate_config() {
 
   # Validate the API credentials
   if [ -z "${secret_id}" ] || [ -z "${secret_key}" ]; then
-    logger -p error -s -t "${LOG_TAG}" "API credentials fields 'SecretId' or 'SecretKey' are missing in '${CONF_FILE}'."
+    logger -p error -s -t "${TAG}" "API credentials fields 'SecretId' or 'SecretKey' are missing in '${CONF_FILE}'."
     return 1
   fi
 
   # Retieve the log file setting
   log_file="$(extract_config LogFile)"
   if [ -z "${log_file}" ]; then
-    logger -p warn -s -t "${LOG_TAG}" "Log file field 'LogFile' missed in in '${CONF_FILE}'."
+    logger -p warn -s -t "${TAG}" "Log file field 'LogFile' missed in in '${CONF_FILE}'."
   else
     LOG_FILE="${log_file}"
   fi
